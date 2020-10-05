9cdc3abcf159d68a8d97a2aba5e7337bb4af14fe
katelynhinz <70711734+katelynhinz@users.noreply.github.com>
2020-10-05 15:05:24 -0500

    Update animals.js

--- MidtermKatie.appstudio/forms/animals/animals.js
+++ MidtermKatie.appstudio/forms/animals/animals.js
@@ -4,5 +4,5 @@ let yourAnimal = prompt("Please choose an animal")
 yourAnimal = yourAnimal.toLowerCase()
 myAnimals.push(yourAnimal)
 let lastAnimal = myAnimals.pop()
-console.log(`The last animal is a ${lastAnimal}.`)
+console.log(`The last animal is a/an ${lastAnimal}.`)
 */
