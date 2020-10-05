5071c8cbed52ee90110c3147952c0dd3916c18dc
katelynhinz <70711734+katelynhinz@users.noreply.github.com>
2020-10-05 14:51:18 -0500

    Create incomeBracket.js

---
+++ MidtermKatie.appstudio/forms/incomeBracket/incomeBracket.js
@@ -0,0 +1,16 @@
+
+btnSubmit.onclick=function showBracket(){
+  let income = Number(inptIncome.value)
+    if (30000 > income) {
+  lblBracket.textContent = (`With your income of ${income}, you are in a tax bracket of 8%`);
+} else if (income >= 30000 && 99999 > income) {
+  lblBracket.textContent = (`With your income of ${income}, you are in a tax bracket of 15%`);
+} else {
+  lblBracket.textContent = (`With your income of ${income}, you are in a tax bracket of 25%`);
+}
+}
+btnGoAgain.onclick=function(){
+  inptIncome.value = ""
+  lblBracket.textContent = ""
+}
+
