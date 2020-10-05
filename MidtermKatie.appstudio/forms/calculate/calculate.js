494e8b1a3df46ef5ed7e9d9f7b2eff13ec3a48c8
katelynhinz <70711734+katelynhinz@users.noreply.github.com>
2020-10-05 14:32:28 -0500

    Create calculate.js

---
+++ MidtermKatie.appstudio/forms/calculate/calculate.js
@@ -0,0 +1,9 @@
+function calcAvgSquare(num1, num2, num3) {
+   return  ((num1 + num2 + num3)/3) * num1**2
+}
+let firstNum = prompt("Please select your first number")
+let secondNum = prompt("Please select your second number")
+let thirdNum = prompt("Please select your third number")
+
+let answer = calcAvgSquare(Number(firstNum), Number(secondNum), Number(thirdNum))
+console.log(`The answer is ${answer}.`)  
