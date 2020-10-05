894a04573185b6e68cadb2246fe96e6bcd855999
katelynhinz <70711734+katelynhinz@users.noreply.github.com>
2020-10-05 14:32:25 -0500

    Create animals.js

---
+++ MidtermKatie.appstudio/forms/animals/animals.js
@@ -0,0 +1,7 @@
+/* myAnimals = ["dog", "cat", "horse", "meerkat"]
+yourAnimal = prompt("Please choose an animal")
+yourAnimal = yourAnimal.toLowerCase()
+newAnimals = myAnimals.push(yourAnimal)
+console.log(newAnimals)
+console.log(`The last animal is a ${newAnimals.pop()}.`)
+*/
