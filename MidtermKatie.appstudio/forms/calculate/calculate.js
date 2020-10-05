7275aaa5084223dd7e850819dde59f3da2f5fd5d
katelynhinz <70711734+katelynhinz@users.noreply.github.com>
2020-10-05 14:51:10 -0500

    Update calculate.js

--- MidtermKatie.appstudio/forms/calculate/calculate.js
+++ MidtermKatie.appstudio/forms/calculate/calculate.js
@@ -1,4 +1,4 @@
-function calcAvgSquare(num1, num2, num3) {
+/* function calcAvgSquare(num1, num2, num3) {
    return  ((num1 + num2 + num3)/3) * num1**2
 }
 let firstNum = prompt("Please select your first number")
@@ -7,3 +7,4 @@ let thirdNum = prompt("Please select your third number")
 
 let answer = calcAvgSquare(Number(firstNum), Number(secondNum), Number(thirdNum))
 console.log(`The answer is ${answer}.`)  
+*/
